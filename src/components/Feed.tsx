ddb20bc234027acf106be0a4bfca123d128e1b26
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2025-03-23 21:25:46 +0700

    Reapply "Applying Prisma, Migrate and Seed, Authentication with Clerk and Fetching data Demo"
    This reverts commit 16735dfa8eab40d6c5f88061d87f0fe9f812b0b3.

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,14 +1,44 @@
-import Post from "./Post"
+import { prisma } from "@/prisma";
+import Post from "./Post";
+import { auth } from "@clerk/nextjs/server";
 
-const Feed = () => {
+const Feed = async ({ userProfileId }: { userProfileId: string }) => {
+  const { userId } = await auth();
+
+  if (!userId) return;
+
+  const whereCondition = userProfileId
+    ? { userId: userProfileId }
+    : {
+      userId: {
+        in: [
+          userId,
+          ...(
+            await prisma.follow.findMany({
+              where: { followerId: userId },
+              select: { followingId: true }
+            })
+          ).map((follow) => follow.followingId),
+        ],
+      },
+    };
+
+  console.log(whereCondition)
+
+  const posts = await prisma.post.findMany({ where: whereCondition })
+
+  console.log(posts)
+
+  // FETCH POSTS FROM THE CURRENT USER AND THE FOLLOWINGS
   return (
-    <div className=''>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+    <div className="">
+      {/* {posts.map(post=>(
+        <div key={post.id}>
+          <Post/>
+        </div>
+      ))} */}
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
