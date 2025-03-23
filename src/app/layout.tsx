16735dfa8eab40d6c5f88061d87f0fe9f812b0b3
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2025-03-23 20:28:43 +0700

    Revert "Applying Prisma, Migrate and Seed, Authentication with Clerk and Fetching data Demo"
    This reverts commit f209acd45f486c6a0df15b6492b648bcf7e1711a.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
+import LeftBar from "@/components/LeftBar";
 import "./globals.css";
+import RightBar from "@/components/RightBar";
 
 import type { Metadata } from 'next'
-import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: 'Lama Dev X Clone',
   description: 'Next.js social media application project',
 }
 
-export default function AppLayout({
+export default function RootLayout({
   children,
   modal
 }: Readonly<{
@@ -16,12 +17,21 @@ export default function AppLayout({
   modal: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
     <html lang="en">
       <body>
-        {children}
+        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
+          <div className="px-2 xsm:px-4 xxl:px-8 ">
+            <LeftBar />
+          </div>
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray ">
+            {children}
+            {modal}
+          </div>
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
+            <RightBar />
+          </div>
+        </div>
       </body>
     </html>
-    </ClerkProvider>
   );
 }
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,44 +1,14 @@
-import { prisma } from "@/prisma";
-import Post from "./Post";
-import { auth } from "@clerk/nextjs/server";
+import Post from "./Post"
 
-const Feed = async ({ userProfileId }: { userProfileId: string }) => {
-  const { userId } = await auth();
-
-  if (!userId) return;
-
-  const whereCondition = userProfileId
-    ? { userId: userProfileId }
-    : {
-      userId: {
-        in: [
-          userId,
-          ...(
-            await prisma.follow.findMany({
-              where: { followerId: userId },
-              select: { followingId: true }
-            })
-          ).map((follow) => follow.followingId),
-        ],
-      },
-    };
-
-  console.log(whereCondition)
-
-  const posts = await prisma.post.findMany({ where: whereCondition })
-
-  console.log(posts)
-
-  // FETCH POSTS FROM THE CURRENT USER AND THE FOLLOWINGS
+const Feed = () => {
   return (
-    <div className="">
-      {/* {posts.map(post=>(
-        <div key={post.id}>
-          <Post/>
-        </div>
-      ))} */}
+    <div className=''>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
     </div>
-  );
-};
+  )
+}
 
-export default Feed;
+export default Feed
